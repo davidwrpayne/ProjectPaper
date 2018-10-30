a60270ef0e768fe878de2144fbb96bb22e7a23ea
David Payne <david.payne@bigcommerce.com>
2018-10-30 12:56:35 -0700

    pushing files up one directory to base directory

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ This project is meant as a way to explore pages on github.
 
 Also exploring hosting javascript code for small project.
 
+
+
---
+++ src/antsim.js
@@ -0,0 +1,7 @@
+
+var canvasName = "main-viewport";
+
+var canvas = document.getElementById(canvasName);
+var ctx = canvas.getContext("2d");
+ctx.fillStyle = "#FF0000";
+ctx.fillRect(0,0,150,75);
