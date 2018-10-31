9940a2418a6ffed11561ccecef062cd3a7c01cf7
David Payne <david.payne@bigcommerce.com>
2018-10-31 08:49:37 -0700

    feat(platform): adding screen drawing on canvas and setup function

--- src/antsim.js
+++ src/antsim.js
@@ -1,7 +1,60 @@
 
-var canvasName = "main-viewport";
 
-var canvas = document.getElementById(canvasName);
-var ctx = canvas.getContext("2d");
-ctx.fillStyle = "#FF0000";
-ctx.fillRect(0,0,150,75);
+// main
+var canvas = null;
+var ctx = null;
+var loaded = false;
+
+function setup() {
+    var canvasName = "main-viewport";
+
+    canvas = document.getElementById(canvasName);
+    ctx = canvas.getContext("2d");
+    console.log("received canvas" . canvas);
+}
+
+
+
+function clearScreen() {
+    ctx.fillStyle = "white";
+    ctx.fillRect(0,0,canvas.width, canvas.height);
+}
+
+
+function update() {
+
+}
+
+function draw() {
+    clearScreen();
+
+    ctx.beginPath();
+    ctx.fillStyle = "black";
+    ctx.lineWidth = 2;
+    ctx.arc(100,100,70,0,2*Math.PI);
+    ctx.stroke();
+}
+
+var lastFrameTimeMs = 0;
+var maxFPS = 60;
+var delta = 0;
+
+function mainLoop(timestamp) {
+    if (!loaded) {
+        setup();
+    }
+
+    // Throttle the frame rate.
+    if (timestamp < lastFrameTimeMs + (1000 / maxFPS)) {
+        requestAnimationFrame(mainLoop);
+        return;
+    }
+    delta = timestamp - lastFrameTimeMs;
+    lastFrameTimeMs = timestamp;
+
+    update(delta);
+    draw();
+    requestAnimationFrame(mainLoop);
+}
+
+window.onload = requestAnimationFrame(mainLoop);
